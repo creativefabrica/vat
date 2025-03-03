6560a376eab8bd77a64ed86413aa0e0427cfd1d7
Pablo Crivella <pablocrivella@gmail.com>
2025-03-03 10:02:58 +0100

    Rephrase error message

--- errors.go
+++ errors.go
@@ -7,6 +7,6 @@ var (
 	ErrNotFound               = errors.New("VAT number not found")
 	ErrServiceUnavailable     = errors.New("validation service unavailable")
 	ErrInvalidCountryCode     = errors.New("invalid country code")
-	ErrUKVATClientNotProvided = errors.New("UK VAT service not provided")
-	ErrViesClientNotProvided  = errors.New("VIES service not provided")
+	ErrUKVATClientNotProvided = errors.New("UK VAT client not provided")
+	ErrViesClientNotProvided  = errors.New("VIES client not provided")
 )
